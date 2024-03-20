c9eea388ba12663633c14a56411a90c8c354a3b5
Ktchoma <164261802+Ktchoma@users.noreply.github.com>
2024-03-21 01:02:57 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# DepartmentFileGallery
+## A Files management system which helps users to upload,download and manage files of their particular department. The whole system is coded in core PHP and MySqli
+
+
+
+
+
+### Installation
+- Upload DepartmentFileGallery to the directory of your choice. (E.g :  /var/www/html/departmentfilegallery )
+- Import MySql Db file to your database software (E.g : PhpMyAdmin -> Create DB -> Import files.sql  )
+- Configure connection between your database and server by modifying the /includes/connection.php file
+- Navigate to the installation in your browser ( E.g :  http://localhost/DepartmentFileGallery )
+- Done :)
+
+
+
+### Main Features
+
+- Multiple user access:  Allows multiple type of users(Manager/Employee/Admin) to login 
+- Functional Admin panel:  Allows admins to manage the whole system
+- CRUD functionalities:  Allows all users to create,read,update and delete their files in a managed format 
+- Profile update option:  Allows users to update their profile/account details  
+- Secure registration and login option for users
+- Allows Employees and Managers to download/upload their Department Files easily
+- Allows users to recover their password using forgot password option
+
+### To-Do  list
+- Add pagination for files
+- Add login with facebook and google+ option
+- Add search files option
+
+### Issues
+
+Please log any issues found in the repository 
+
+
---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+
+</head>
+<body>
+    <div class="about-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="about-img">
+                        <img src="images/back.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 col-lg-offset-1">
+                    <div class="about-text">
+                        <h2>About Us</h2>
+                        <p>Welcome to Departments File Gallery, the premier file management solution tailored for diverse departments and teams! Departments File Gallery is the brainchild of a passionate  innovator who recognized the need for a specialized platform that caters to the unique file-sharing and collaboration requirements of various departments within organizations.
+
+
+Managers can now streamline their workflows by securely uploading and managing important files, ensuring that their teams have access to the latest information. The intuitive interface facilitates easy deletion of obsolete files, maintaining a clutter-free and organized digital workspace. Departments File Gallery's advanced permission settings enable managers to control access levels, ensuring that sensitive information is shared only with the right individuals.
+
+For employees, Departments File Gallery is a collaborative haven where they can contribute, access, and view files with ease. Sharing insights and collaborating on projects has never been more straightforward.
+
+Departments File Gallery is not just a file management tool; it's a catalyst for efficiency, productivity, and collaboration within organizations!</p>
+                        <a href="#">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    
